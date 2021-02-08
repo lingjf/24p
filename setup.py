feecb743a1c022a0e45360c13c80bddef8eabf0f
凌建发 <jianfa.ljf@alibaba-inc.com>
2021-02-08 23:50:31 +0800

    publish npm

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="24p",
-    version="0.0.8",
+    version="0.1.0",
     author="lingjf",
     author_email="lingjf@gmail.com",
     description="24Point",
